aa06a8b4cf315c0e567ef174ad14d3850e179215
IT14KR <131267129+IT14KR@users.noreply.github.com>
2023-07-29 16:41:06 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+const LS_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const currentTime = data.seconds;
+    localStorage.setItem(LS_KEY, currentTime);
+  }, 1000)
+);
+
+const iframe = document.querySelector('iframe');
+
+const saveTime = localStorage.getItem(LS_KEY);
+
+if (saveTime !== null) {
+  player.setCurrentTime(saveTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  inputEmail: document.querySelector('input'),
+  textMassage: document.querySelector('textarea'),
+  form: document.querySelector('.feedback-form'),
+};
+
+const LS_KEY = 'feedback-form-state';
+
+refs.form.addEventListener('input', getVelueForm);
+
+function getVelueForm() {
+  const feedbackSave = {
+    email: refs.inputEmail.value,
+    message: refs.textMassage.value,
+  };
+  localStorage.setItem(LS_KEY, JSON.stringify(feedbackSave));
+}
+
+document.addEventListener('DOMContentLoaded', getVelueFromLS);
+
+function getVelueFromLS() {
+  const saveFeedback = localStorage.getItem(LS_KEY);
+  if (saveFeedback) {
+    const feedbackSave = JSON.parse(saveFeedback);
+    refs.inputEmail.value = feedbackSave.email;
+    refs.textMassage.value = feedbackSave.message;
+  }
+}
+
+function pressSubmit(event) {
+  const feedbackSave = {
+    email: refs.inputEmail.value,
+    message: refs.textMassage.value,
+  };
+
+  localStorage.clear();
+  refs.form.reset();
+}
+
+const saveTimeToLS = throttle(currentTime => {
+  localStorage.setItem(LS_KEY, currentTime);
+}, 500);
+
+refs.form.addEventListener('submit', pressSubmit);
+
+// - логіка яка при завантаженні сторіки перевіряє сховище JSON pars і підставляє в форму
+
+// - в кінці очищувати форму очищувати сховище
